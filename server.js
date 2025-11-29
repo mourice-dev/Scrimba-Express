8f537885df13ec416159fdf823dab38a62fb525c
Nshuti Maurice <nshutikope@gmail.com>
2025-11-29 08:59:08 +0200

    adds

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 /** @format */
 
 import express from "express";
+import { startup } from './data/data.js'
 
-const celebrity = {
-  type: "action hero",
-  name: "nshut maurice",
-};
+// const celebrity = {
+//   type: "action hero",
+//   name: "nshut maurice",
+// };
+const PORT = 8000;
 
 const app = express();
 
 app.get("/", (req, res) => {
+    res.send("Server is running. Go to /api to see data.");
+});
+
+app.get("/api", (req, res) => {
   console.log("Root route hit");
-  res.json(celebrity);
+  res.json(startup);
 });
 
 
-app.listen(8000, () => {
-  console.log(`server connected! 8000`);
+app.listen(PORT, () => {
+  console.log(`server connected! ${PORT}`);
 });
