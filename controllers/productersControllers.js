cee213f33eb0e3b97a97113516ec3cda2ec04324
Nshuti Maurice <nshutikope@gmail.com>
2025-12-03 23:58:24 +0200

    ft/select-quert-gerne

--- controllers/productersControllers.js
+++ controllers/productersControllers.js
@@ -20,15 +20,32 @@ export async function getProducts(req, res) {
   try {
     const db = await getDBConnection();
 
+    let query = "SELECT * FROM products";
+
     /*
 Challenge:
-1. Write logic in getProducts() so all products display on page load.
-	 
-   As we will need to modify it in the next challenge, store the SQL query in a let and pass it into the all() method.
-*/ let sql = "SELECT * FROM products";
-    const products = await db.all(sql);
-    
-    res.json(products);
+1. Detect if a query string ‘genre’ is used. 
+   If it is, retrieve only products with that genre from the database and serve them. 
+   If not, all products should be served.
+
+hint.md for help
+
+Example incoming query: '?genre=rock'
+*/
+    const genre = req.query;
+    genre.map((g) => g.genre);
+
+    if (genre === "/genres") {
+
+     query = "SELECT genre FROM products";
+      const products = await db.all(query);
+      res.json(products);
+
+    } else {
+      
+      const products = await db.all(query);
+      res.json(products);
+    }
   } catch (err) {
     res
       .status(500)
@@ -36,7 +53,55 @@ Challenge:
   }
 }
 
+// import { getDBConnection } from '../db/db.js'
 
+// export async function getGenres(req, res) {
+
+//   try {
+
+//     const db = await getDBConnection()
+
+//     const genreRows = await db.all('SELECT DISTINCT genre FROM products')
+//     const genres = genreRows.map(row => row.genre)
+//     res.json(genres)
+
+//   } catch (err) {
+
+//     res.status(500).json({error: 'Failed to fetch genres', details: err.message})
+
+//   }
+// }
+
+// export async function getProducts(req, res) {
+
+//   try {
+
+//     const db = await getDBConnection()
+
+//     let query = 'SELECT * FROM products'
+
+// /*
+// Challenge:
+// 1. Detect if a query string ‘genre’ is used.
+//    If it is, retrieve only products with that genre from the database and serve them.
+//    If not, all products should be served.
+
+// hint.md for help
+
+// Example incoming query: '?genre=rock'
+// */
+
+//     const products = await db.all(query)
+
+//     res.json(products)
+
+//   } catch (err) {
+
+//     res.status(500).json({error: 'Failed to fetch products', details: err.message})
+
+//   }
+
+// }
 
 // /** @format */
 // import { getDBConnection } from "../db/db.js";
